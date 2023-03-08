521be4fbaec8fa02c259f20e9e2f3ce131fe4229
Akr <dksrlfks6079@naver.com>
2023-03-08 09:49:12 +0900

    mynote-app

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function header() {
+    return (
+        <div className='header'>
+            <h1>My Note</h1>
+            <button>글쓰기</button>
+        </div>
+    )
+}
